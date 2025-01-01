ca7ab3ad75aa1934e28564ef5cf4e6dabc032e43
Aspirata <Const.rv2007@gmail.com>
2025-01-01 17:28:02 +0700

    Fixed some things

--- start_me_win.bat
+++ start_me_win.bat
@@ -3,9 +3,8 @@
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
     echo Python is not installed. Starting installation...
-    start /wait Scripts\python-3.10.11-amd64.exe /passive InstallAllUsers=1 PrependPath=1
+    start /wait Scripts\python-3.13_installer.exe /passive InstallAllUsers=1 PrependPath=1
     echo Installation completed.
-    pause
     start "" "start_me_win.bat"
     exit
     
